dc21ecb02bb7cea731e81e0e65503c950e7e0975
fakmalpradana <fakmalpradana@gmail.com>
2023-06-29 15:06:48 +0700

    create element for navbar

---
+++ aita/src/template/nav.tsx
@@ -0,0 +1,34 @@
+import { headers } from "next/dist/client/components/headers"
+import Link from "next/link"
+import '../styles/header-module.css'
+ 
+export default function Header() {
+  return(
+    <header className="container">
+      <div>
+        <ul className="list">
+          <li className="item">
+            <Link href="/">
+              Home
+            </Link>
+          </li>
+          <li className="item">
+            <Link href="/">
+              Product
+            </Link>
+          </li>
+          <li className="item">
+            <Link href="/">
+              About Us
+            </Link>
+          </li>
+          <li className="item brown">
+            <Link href="/">
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </header>
+  )
+}
